72858383da536f79631526ffaec24dffbdcff6ef
Stakshi <semwaleishta6@gmail.com>
2025-05-10 17:44:45 +0530

    Update lockfile and config for deployment stability

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY,
+  },
 };
 
 module.exports = nextConfig; 
